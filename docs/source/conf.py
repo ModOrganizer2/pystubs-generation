c60d1ac92f42de9a9cffa86064c76f4d9060f017
Mikaël Capelle <mikael.capelle@irt-saintexupery.com>
2020-09-24 16:15:27 +0200

    Add favicon.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,6 +64,8 @@ html_sidebars = {
     "**": ["globaltoc.html", "relations.html", "sourcelink.html", "searchbox.html"]
 }
 
+html_favicon = "mo2.ico"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
